b80d5a2484cc2120e016a5bb65bf7e08420877f9
Julio Storch <julio.storch@netknights.it>
2024-05-22 12:13:44 +0200

    Fixes and add docs

--- toolbox/check_certificates.py
+++ toolbox/check_certificates.py
@@ -10,6 +10,95 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 import logging
 
+__doc__ = """
+Documentation of the Certificate Check Script
+
+Overview:
+
+This script is used to check TLS certificates for web servers and LDAP servers,
+as well as CA certificates. It allows you to check the validity of certificates
+and issue warnings if a certificate is about to expire.
+The script can optionally create log files.
+
+Usage:
+
+The script is executed from the command line
+and supports several arguments to configure the certificate checks.
+
+Command Line Arguments:
+
+- `--days`: (required) Number of days before expiration to issue a warning.
+- `--config-dir`: Directory to search for web server configuration files.
+- `--web`: Checks the web server certificate.
+- `--ldap`: Checks the LDAP server certificate.
+- `--ca`: Checks the CA certificate.
+- `--all`: Checks all web server, LDAP server, and CA certificates.
+- `--logging`: Path to the log file. If not set, logs will be printed to stdout.
+
+
+Example Invocations:
+
+1. Check all certificates and log to a file (--all include this arguments: --web --ldap --ca):
+   python check_certificates.py --days 30 --all --logging /path/to/logfile.log
+
+2. Check only web server certificates and output to console:
+   python check_certificates.py --days 30 --web
+
+3. Check LDAP server certificates and log to a file:
+   python check_certificates.py --days 30 --ldap --logging /path/to/logfile.log
+
+4. Check CA certificates and output to console:
+   python check_certificates.py --days 30 --ca
+
+
+Example for stdout or logging:
+
+1. check_certificates.py --days 30 --all 2> /var/log/privacyidea/all_certificates.log
+or
+2. check_certificates.py --days 30 --all --logging /var/log/privacyidea/all_certificates.log
+
+
+Sample Output:
+
+When the script runs, the output may look like this:
+
+2024-05-22 09:54:05,621 - INFO - Web server certificate is valid for 1333 more days.
+
+2024-05-22 09:54:07,088 - INFO - LDAP server from resolver "ad" certificate is valid for 123 more days.
+2024-05-22 09:54:07,089 - INFO - CA issuer from resolver "ad" certificate is valid for 614 more days.
+2024-05-22 09:54:07,092 - INFO - The LDAP server ad certificate is validly signed by its issuer.
+2024-05-22 09:54:07,175 - INFO - LDAP server from resolver "ucs07" certificate is valid for 1369 more days.
+2024-05-22 09:54:07,249 - INFO - LDAP server from resolver "ucs05" certificate is valid for 878 more days.
+
+2024-05-17 13:39:15,315 - INFO - CA issuer from resolver "ad" certificate is valid for 619 more days.
+2024-05-17 13:39:15,316 - INFO - CA issuer from resolver "ucs07" certificate is valid for 1373 more days.
+2024-05-17 13:39:15,316 - INFO - CA issuer from resolver "ucs05" certificate is valid for 883 more days.
+
+
+Error Handling:
+
+The script handles various types of errors,
+such as failing to retrieve certificates or
+problems reading configuration files, and outputs corresponding error messages:
+
+unable to load certificate
+140293905839424:error:0909006C:PEM routines:get_name:no start line:../crypto/pem/pem_lib.c:745:
+Expecting: TRUSTED CERTIFICATE
+2024-05-22 09:54:06,976 - WARNING - Failed to retrieve certificate from 10.10.10.10:389. Trying connection check...
+2024-05-22 09:54:07,021 - ERROR - Failed to connect to 10.10.10.10:389:
+Command 'echo | openssl s_client -connect 10.10.10.10:389 2>/dev/null' returned non-zero exit status 1.
+2024-05-22 09:54:07,022 - WARNING - No certificate found for LDAP server from resolver "ldap_test" at 10.10.10.10:389
+
+
+Summary:
+
+This script is a helpful tool for administrators to monitor the validity
+of TLS certificates in their systems and receive timely warnings when
+certificates need to be renewed.
+By supporting log files and various operating modes,
+it can be flexibly integrated into different monitoring and maintenance processes.
+"""
+
 
 def setup_logging(log_path=None):
     """
@@ -18,7 +107,8 @@ def setup_logging(log_path=None):
     """
     log_format = '%(asctime)s - %(levelname)s - %(message)s'
     if log_path:
-        logging.basicConfig(filename=log_path, level=logging.INFO, format=log_format)
+        logging.basicConfig(
+            filename=log_path, level=logging.INFO, format=log_format)
     else:
         logging.basicConfig(level=logging.INFO, format=log_format)
 
@@ -81,14 +171,16 @@ def load_certificates(cert_path, ca_path=None):
         if cert_path:
             with open(cert_path, 'rb') as file:
                 cert = x509.load_pem_x509_certificate(file.read(), default_backend())
+
         ca_cert = None
         if ca_path:
             with open(ca_path, 'rb') as file:
                 ca_cert = x509.load_pem_x509_certificate(file.read(), default_backend())
+
         return cert, ca_cert
     except Exception as e:
-        log_message(f"Failed to load certificates from "
-                    f"{cert_path} or {ca_path}: {str(e)}", error=True)
+        log_message(
+            f"Failed to load certificates from {cert_path} or {ca_path}: {str(e)}", error=True)
         return None, None
 
 
@@ -104,8 +196,10 @@ def check_certificate_expiry(cert, days, cert_description):
         message = f'{cert_description} certificate is valid for {days_to_expire} more days.'
         log_message(message)
         if days_to_expire <= days:
-            log_message(f"Warning: The {cert_description} certificate will expire in "
-                        f"{days_to_expire} days or less. Please renew it timely.", warning=True)
+            log_message(
+                f"Warning: The {cert_description} certificate will expire in {days_to_expire} days or less. "
+                "Please renew it timely.",
+                warning=True)
 
 
 def get_certificate_from_server(server_address, port, starttls=False):
@@ -118,28 +212,28 @@ def get_certificate_from_server(server_address, port, starttls=False):
     """
     try:
         if starttls:
-            cmd = (f"echo | openssl s_client -connect {server_address}:{port} "
-                   f"-starttls ldap 2>/dev/null | openssl x509")
+            cmd = (
+                f"echo | openssl s_client -connect {server_address}:{port} -starttls ldap "
+                "2>/dev/null | openssl x509")
         else:
-            cmd = (f"echo | openssl s_client -connect {server_address}:{port} "
-                   f"2>/dev/null | openssl x509")
+            cmd = f"echo | openssl s_client -connect {server_address}:{port} 2>/dev/null | openssl x509"
         cert_pem = subprocess.check_output(cmd, shell=True)
         cert = x509.load_pem_x509_certificate(cert_pem, default_backend())
         return cert
-    except subprocess.CalledProcessError as e:
-        # If failed to retrieve certificate, try checking connection
-        log_message(f"Failed to retrieve certificate from {server_address}:{port}. "
-                    f"Trying connection check...", warning=True)
+    except subprocess.CalledProcessError:
+        log_message(
+            f"Failed to retrieve certificate from {server_address}:{port}. Trying connection check...",
+            warning=True)
         try:
             cmd = f"echo | openssl s_client -connect {server_address}:{port} 2>/dev/null"
             connection_output = subprocess.check_output(cmd, shell=True).decode('utf-8')
             if "CONNECTED(00000003)" in connection_output:
-                log_message(f"Connection to {server_address}:{port} successful, "
-                            f"but no certificate found.", warning=True)
+                log_message(
+                    f"Connection to {server_address}:{port} successful, but no certificate found.",
+                    warning=True)
                 return None
             else:
-                log_message(f"Failed to establish connection to "
-                            f"{server_address}:{port}.", error=True)
+                log_message(f"Failed to establish connection to {server_address}:{port}.", error=True)
                 return None
         except subprocess.CalledProcessError as ce:
             log_message(f"Failed to connect to {server_address}:{port}: {str(ce)}", error=True)
@@ -161,34 +255,39 @@ def verify_certificate_signature(client_cert, ca_cert, cert_description):
             client_cert.signature_hash_algorithm)
         log_message(f"The {cert_description} certificate is validly signed by its issuer.")
     except Exception as e:
-        log_message(f"Verification failed: The {cert_description} certificate "
-                    f"is not properly signed by its issuer: {str(e)}", error=True)
+        log_message(
+            f"Verification failed: The {cert_description} certificate is not properly signed by its issuer: "
+            f"{str(e)}",
+            error=True)
 
 
 def main():
     """
     Main function to check TLS certificates and issue warnings if they are about to expire.
     """
-    parser = argparse.ArgumentParser(description='Check TLS certificates and issue a warning '
-                                                 'if expiration is imminent.')
-    parser.add_argument('--days', type=int, required=True, help='Number of days before expiration '
-                                                                'to issue a warning.')
-    parser.add_argument('--config-dir', type=str, help='Directory to search '
-                                                       'for web server config files.')
-    parser.add_argument('--web', action='store_true', help='Check the web server certificate.')
-    parser.add_argument('--ldap', action='store_true', help='Check the LDAP server certificate.')
-    parser.add_argument('--ca', action='store_true', help='Check the CA issuer certificate.')
-    parser.add_argument('--all', action='store_true', help='Check all web, LDAP, '
-                                                           'and CA issuer certificates.')
-    parser.add_argument('--logging', type=str, help='Path to the log file. '
-                                                    'If not set, logs will be printed to stdout.')
+    parser = argparse.ArgumentParser(
+        description='Check TLS certificates and issue a warning if expiration is imminent.')
+    parser.add_argument(
+        '--days', type=int, required=True, help='Number of days before expiration to issue a warning.')
+    parser.add_argument(
+        '--config-dir', type=str, help='Directory to search for web server config files.')
+    parser.add_argument(
+        '--web', action='store_true', help='Check the web server certificate.')
+    parser.add_argument(
+        '--ldap', action='store_true', help='Check the LDAP server certificate.')
+    parser.add_argument(
+        '--ca', action='store_true', help='Check the CA issuer certificate.')
+    parser.add_argument(
+        '--all', action='store_true', help='Check all web, LDAP, and CA issuer certificates.')
+    parser.add_argument(
+        '--logging', type=str, help='Path to the log file. If not set, logs will be printed to stdout.')
     args = parser.parse_args()
 
     # Setup logging based on the provided argument
     setup_logging(args.logging)
 
     # Enable CA checks if --all is specified
-    ca_checks = args.ca
+    ca_checks = args.ca or args.all
 
     # Web server certificates check
     if args.web or args.all:
@@ -199,6 +298,7 @@ def main():
             "nginx": "/etc/nginx/sites-enabled"}
         if args.config_dir:
             config_dirs = {"custom": args.config_dir}
+
         for name, directory in config_dirs.items():
             config_paths = list(find_config_files(directory, file_patterns))
             for path in config_paths:
@@ -221,28 +321,26 @@ def main():
                         scheme, server_info = uri_parts
                         server_info = server_info.split(":")
                         server_address = server_info[0]
-                        port = server_info[1] if (len(server_info) ==
-                                                  2) else (636 if scheme == "ldaps" else 389)
+                        port = server_info[1] if len(server_info) == 2 else (636 if scheme == "ldaps" else 389)
                         starttls = scheme == "ldap"
                         cert = get_certificate_from_server(server_address, port, starttls)
                         if cert:
-                            check_certificate_expiry(cert, args.days, f'LDAP server '
-                                                                      f'from resolver "{resolver_name}"')
+                            check_certificate_expiry(
+                                cert, args.days, f'LDAP server from resolver "{resolver_name}"')
                         else:
-                            log_message(f"No certificate found for LDAP server "
-                                        f"from resolver \"{resolver_name}\" "
-                                        f"at {server_address}:{port}", warning=True)
-                        # Check CA certificate if TLS_VERIFY is set
+                            log_message(
+                                f"No certificate found for LDAP server from resolver \"{resolver_name}\" at "
+                                f"{server_address}:{port}", warning=True)
                         if resolver_data["data"].get("TLS_VERIFY", "").lower() == "true":
                             ca_path = resolver_data["data"].get("TLS_CA_FILE")
                             if ca_path:
                                 _, ca_cert = load_certificates(cert_path=None, ca_path=ca_path)
                                 if ca_cert:
-                                    check_certificate_expiry(ca_cert, args.days,
-                                                             f'CA issuer from resolver "{resolver_name}"')
+                                    check_certificate_expiry(
+                                        ca_cert, args.days, f'CA issuer from resolver "{resolver_name}"')
                                     if cert and ca_cert:
-                                        verify_certificate_signature(cert, ca_cert,
-                                                                     f'LDAP server {resolver_name}')
+                                        verify_certificate_signature(
+                                            cert, ca_cert, f'LDAP server {resolver_name}')
         except subprocess.CalledProcessError as e:
             log_message(f"Failed to execute command: {str(e)}", error=True)
         except json.JSONDecodeError:
@@ -250,7 +348,6 @@ def main():
         except Exception as e:
             log_message(f"An unexpected error occurred: {str(e)}", error=True)
 
-    # CA issuer certificates check
     if ca_checks:
         try:
             cmd = "pi-manage config exporter -t resolver -f json"
@@ -262,8 +359,8 @@ def main():
                     if ca_path:
                         _, ca_cert = load_certificates(cert_path=None, ca_path=ca_path)
                         if ca_cert:
-                            check_certificate_expiry(ca_cert, args.days,
-                                                     f'CA issuer from resolver "{resolver_name}"')
+                            check_certificate_expiry(
+                                ca_cert, args.days, f'CA issuer from resolver "{resolver_name}"')
         except subprocess.CalledProcessError as e:
             log_message(f"Failed to execute command: {str(e)}", error=True)
         except json.JSONDecodeError:
